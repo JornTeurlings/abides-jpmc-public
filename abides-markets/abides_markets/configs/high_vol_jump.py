6f99cfb0fc9e6701b8cfda422164d03bed6f04ba
Jorn Teurlings <jornteurlings@icloud.com>
2025-08-16 18:36:53 +0200

    More filler information removed

--- abides-markets/abides_markets/configs/high_vol_jump.py
+++ abides-markets/abides_markets/configs/high_vol_jump.py
@@ -1,10 +1,3 @@
-# RMSC-4 (Reference Market Simulation Configuration):
-# - 1     Exchange Agent
-# - 2     Adaptive Market Maker Agents
-# - 102   Value Agents
-# - 12    Momentum Agents
-# - 1000  Noise Agents
-
 import os
 from datetime import datetime
 
